dd8e3d29ae582b84663100477cbdfc020320271c
Tristan Rimmer <Tristan Rimmer>
2026-08-04 14:06:23 +0100

    Implemented a funcion for determining the correct physical device

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "glfw_window_handler.hh"
+#include <vulkan/vulkan.hpp>
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
 #else
@@ -13,15 +14,16 @@ import vulkan_hpp;
 #include <iostream>
 #include <stdexcept>
 
-constexpr uint32_t WIDTH = 800;
-constexpr uint32_t HEIGHT = 600;
+const std::vector<const char *> REQUIRED_DEVICE_EXTENSIONS = {
+    vk::KHRSwapchainExtensionName};
 
-// class VulkanHandler {
-// public:
-// private:
-//   vk::raii::Context context;
-//   vk::raii::Instance instance = nullptr;
-// };
+const std::vector<char const *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+#ifdef NDEBUG
+constexpr bool ENABLE_VALIDATION_LAYERS = false;
+#else
+constexpr bool ENABLE_VALIDATION_LAYERS = true;
+#endif
 
 class HelloTriangleApplication {
 public:
@@ -32,11 +34,13 @@ public:
   void run() {
     initVulkan();
     mainLoop();
-    cleanup();
   }
 
 private:
-  void initVulkan() { createInstance(); }
+  void initVulkan() {
+    createInstance();
+    pickPhysicalDevice();
+  }
 
   void mainLoop() {
     if (auto window = window_container->get().lock()) {
@@ -49,14 +53,137 @@ private:
     }
   }
 
-  void cleanup() {}
+  /*
+   * This function will find all possible devices that can run vulkan
+   *
+   * it will:
+   *
+   * - try choose a discrete gpu (not integrated graphics)
+   *
+   * - confirm the gpu supports the right api version
+   * - confirm the gpu supports the right command queues
+   * - confirm the gpu supports the needed extensions
+   * - confirm the gpu has the right features
+   */
+  void pickPhysicalDevice() {
+    if (instance == nullptr)
+      throw std::runtime_error(
+          "VK Instance has not been initialised before trying to use it");
 
+    auto all_physical_devices = instance.enumeratePhysicalDevices();
+
+    if (all_physical_devices.empty())
+      throw std::runtime_error(
+          "There are no physical devices with vulkan support");
+
+    for (auto _physical_device : all_physical_devices) {
+      auto device_properties = _physical_device.getProperties();
+      auto device_features = _physical_device.getFeatures();
+
+      // TODO: this function can be a lot more intelligent, ranking gpus and
+      // such. For now, this will just pick the first DISCRETE GPU
+      if (device_properties.deviceType ==
+              vk::PhysicalDeviceType::eDiscreteGpu &&
+          device_features.geometryShader) {
+        this->physical_device = _physical_device;
+        break;
+      }
+    }
+
+    if (!(physical_device.getProperties().apiVersion >= vk::ApiVersion13))
+      throw std::runtime_error("Gpu API version does not meet api version");
+
+    // Queue Stuff now
+    /*
+     * Every operation must be submitted to a queue, and there are different
+     * queue families. Each family will only allow a certain subset of commands.
+     *
+     * For this context, we need to check that the devices supported queues
+     * include those that support the desired properties
+     */
+
+    auto queue_families = physical_device.getQueueFamilyProperties();
+
+    if (!(std::ranges::any_of(queue_families, [](auto const &q_fam_props) {
+          return static_cast<bool>(q_fam_props.queueFlags &
+                                   vk::QueueFlagBits::eGraphics);
+        })))
+      throw std::runtime_error(
+          "Device does not support graphics-oriented queues");
+
+    // Similarly, we alsoneed to ensure it supports the right extensions
+    auto available_device_extensions =
+        physical_device.enumerateDeviceExtensionProperties();
+
+    if (!(std::ranges::all_of(
+            REQUIRED_DEVICE_EXTENSIONS,
+            [&available_device_extensions](auto const &required_deve_ext) {
+              return std::ranges::any_of(
+                  available_device_extensions,
+                  [required_deve_ext](auto const &avail_dev_ext) {
+                    return strcmp(avail_dev_ext.extensionName,
+                                  required_deve_ext) == 0;
+                  });
+            })))
+      throw std::runtime_error(
+          "The device does not support the right extensions");
+
+    // Finally, confirm that it supports all the right features
+    auto device_features = physical_device.template getFeatures2<
+        vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan11Features,
+        vk::PhysicalDeviceVulkan13Features,
+        vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
+
+    // clang-format off
+    if (!(
+          device_features.template get<vk::PhysicalDeviceVulkan11Features>().shaderDrawParameters &&
+          device_features.template get<vk::PhysicalDeviceVulkan13Features>().dynamicRendering &&
+          device_features.template get<vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>().extendedDynamicState
+         ))
+      throw std::runtime_error("The device does not support the right features");
+    // clang-format on
+  }
+
+  /*
+   * So far:
+   * - Gets app information
+   *
+   * - Gets required validation layers - a validation layer is an optional
+   * component 'that hook into vulkan function calls to apply additional
+   * operations'
+   *
+   * - Gets the required extensions from GLFW - the extensions specify how to
+   * interface the vulkan driver with the window system. GLFW provides this
+   *
+   * - Finally creates the vulkan instance
+   */
   void createInstance() {
     vk::ApplicationInfo appInfo{.pApplicationName = "Hello World Triangle",
                                 .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
                                 .pEngineName = "No Engine",
                                 .engineVersion = VK_MAKE_VERSION(1, 0, 0),
                                 .apiVersion = vk::ApiVersion13};
+
+    // Get required layers
+    std::vector<char const *> requiredLayers;
+    if (ENABLE_VALIDATION_LAYERS)
+      requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+
+    // Check if the required layers are supported by the existing vulkan
+    // implementation
+    auto layerProperties = context.enumerateInstanceLayerProperties();
+    // TODO: better understand what this is doing
+    auto unsupportedLayerIterator = std::ranges::find_if(
+        requiredLayers, [&layerProperties](auto const &requiredLayer) {
+          return std::ranges::none_of(
+              layerProperties, [requiredLayer](auto const &layerProperty) {
+                return strcmp(layerProperty.layerName, requiredLayer) == 0;
+              });
+        });
+
+    if (unsupportedLayerIterator != requiredLayers.end())
+      throw std::runtime_error("Required layer is not supported: " +
+                               std::string(*unsupportedLayerIterator));
 
     // Get the required instance extensions from GLFW
     uint32_t glfwExtensionCount = 0;
@@ -83,6 +210,8 @@ private:
     // Also need the global extensions we need
     vk::InstanceCreateInfo createInfo{
         .pApplicationInfo = &appInfo,
+        .enabledLayerCount = static_cast<uint32_t>(requiredLayers.size()),
+        .ppEnabledLayerNames = requiredLayers.data(),
         .enabledExtensionCount = glfwExtensionCount,
         .ppEnabledExtensionNames = glfwExtensions,
     };
@@ -95,9 +224,13 @@ private:
 
   vk::raii::Context context;
   vk::raii::Instance instance = nullptr;
+  vk::raii::PhysicalDevice physical_device = nullptr;
 };
 
 int main() {
+  constexpr uint32_t WIDTH = 800;
+  constexpr uint32_t HEIGHT = 600;
+
   try {
     GlfwWindowContainer container({WIDTH, HEIGHT}, "Test GLFW Window");
     HelloTriangleApplication app(&container);
